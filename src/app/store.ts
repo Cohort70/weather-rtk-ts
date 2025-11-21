d149178a6864c835523dd7675821873e6bf04d98
edward <edwardlfish@gmail.com>
2025-11-21 16:54:09 +0200

    Refactored weather feature to use RTK Query and replaced message and weather slices with city slice. Updated components accordingly.

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
-import weather from "../features/weather/weatherSlice.ts";
-import message from "../features/message/messageSlice.ts";
+import city from "../features/city/citySlice";
+import {weatherApi} from "../features/api/weatherAction.ts";
 
 export const store = configureStore({
     reducer: {
-        weather, message
+        city,
+        [weatherApi.reducerPath]: weatherApi.reducer,
     },
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(weatherApi.middleware)
 })
 
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
