858ce244d51bfbc79ce728ac45d4f34f68344e72
edward <edwardlfish@gmail.com>
2025-11-23 22:15:37 +0200

    Enhanced `getWeatherByCity` endpoint with response transformation and updated type definitions for weather API integration. Simplified unused code.

--- src/features/api/weatherAction.ts
+++ src/features/api/weatherAction.ts
@@ -1,32 +1,23 @@
 import {api_key, base_url} from "../../utils/constants.ts";
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import type {WeatherInfo, WeatherInfoResponse} from "../../utils/types";
 
 export const weatherApi = createApi({
     reducerPath: 'weatherApi',
     baseQuery: fetchBaseQuery({baseUrl: base_url}),
     endpoints: builder => ({
-        getWeatherByCity: builder.query({
-            query: city => `?q=${city}&appid=${api_key}&units=metric`
+        getWeatherByCity: builder.query<WeatherInfo, string>({
+            query: city => `?q=${city}&appid=${api_key}&units=metric`,
+            keepUnusedDataFor: 10,
+            transformResponse: (data: WeatherInfoResponse) => ({
+                city: data.name,
+                temp: data.main.temp,
+                pressure: data.main.pressure,
+                country: data.sys.country,
+                sunset: new Date(data.sys.sunset * 1000).toLocaleTimeString()
+            })
         })
     })
 })
 
 export const {useGetWeatherByCityQuery} = weatherApi;
-
-// export const fetchWeather = createAsyncThunk(
-//     'fetch/weather',
-//     async (city: string) => {
-//         const response = await fetch(`${base_url}?q=${city}&appid=${api_key}&units=metric`)
-//         if(!response.ok){
-//             throw new Error('Enter correct city name')
-//         }
-//         const data = await response.json();
-//         return {
-//             country: data.sys.country,
-//             city: data.name,
-//             temp: data.main.temp,
-//             pressure: data.main.pressure,
-//             sunset: new Date(data.sys.sunset * 1000)
-//         }
-//     }
-// )
--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -3,5 +3,17 @@ export interface WeatherInfo {
     city: string,
     temp: number,
     pressure: number,
-    sunset: Date
+    sunset: string
+}
+
+export interface WeatherInfoResponse {
+    name: string,
+    main: {
+        temp: number,
+        pressure: number
+    },
+    sys: {
+        sunset: number,
+        country: string,
+    },
 }
